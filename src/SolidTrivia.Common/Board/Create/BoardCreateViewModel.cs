cdc693f94680c653b4e755412b600ec6c3ad24b7
Adam Vincent <entityadam@outlook.com>
2020-03-08 01:12:30 -0500

    more stuff

---
+++ src/SolidTrivia.Common/Board/Create/BoardCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using SolidTrivia.Questions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidTrivia.Common.Board.Create
+{
+    public class BoardCreateViewModel
+    {
+        private readonly IQuestionFacade facade;
+
+        public BoardCreateViewModel(IQuestionFacade facade)
+        {
+            this.facade = facade;
+        }
+
+        public void Create() => facade.CreateBoard("");
+
+
+    }
+
+    public class BoardCreateModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/SolidTrivia.Common/Question/Create/QuestionCreateViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidTrivia.Common.Question.Create
+{
+    public class QuestionCreateViewModel
+    {
+    }
+}
